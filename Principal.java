29767be6ddd5ff058cd70702be0b3a948dd306f9
herman <herman@hermanyauck>
2024-05-25 00:33:01 -0300

    1

--- Principal.java
+++ Principal.java
@@ -1,25 +1,27 @@
 import com.google.gson.Gson;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
-
+import java.time.Duration;
 
 public class Principal {
    public static void main(String[] args){
-       System.out.println("hola");
-       URI direccion = URI.create("https://swapi.py4e.com/api/films/1");
-
-       HttpClient client = HttpClient.newHttpClient();
-       HttpRequest request = HttpRequest.newBuilder()
-               .uri(direccion)
-               .build();
-
        try {
+           HttpClient client = HttpClient.newHttpClient();
+           HttpRequest request = HttpRequest.newBuilder()
+                   .uri(URI.create("http://swapi.py4e.com/api/films/1/"))
+                   .timeout(Duration.ofMinutes(2))
+                   .build();
            HttpResponse<String> response = client
                    .send(request, HttpResponse.BodyHandlers.ofString());
-           return new Gson().fromJson(response.body(), Pelicula.class);
+
+           System.out.println(response);
+           System.out.println(response.body());
+           Pelicula peli = new Gson().fromJson(response.body(), Pelicula.class);
+           System.out.println("GSON es el sigueinte:");
+           System.out.println(peli);
        } catch (Exception e) {
            throw new RuntimeException("No encontré esa película.");
        }
