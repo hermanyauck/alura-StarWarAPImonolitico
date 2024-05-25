3f37986c5af8c41b13984a33223ccce8b67fcd5c
herman <herman@hermanyauck>
2024-05-25 00:50:14 -0300

    1

---
+++ BuscarPelicula.java
@@ -0,0 +1,26 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class BuscarPelicula {
+
+    public Pelicula busquedaPelicula(int numero){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://swapi.py4e.com/api/films/"+numero+"/"))
+                .timeout(Duration.ofMinutes(1))
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa película.");
+        }
+    }
+}
--- Principal.java
+++ Principal.java
@@ -1,29 +1,18 @@
 import com.google.gson.Gson;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.Scanner;
 
 public class Principal {
    public static void main(String[] args){
-       try {
-           HttpClient client = HttpClient.newHttpClient();
-           HttpRequest request = HttpRequest.newBuilder()
-                   .uri(URI.create("http://swapi.py4e.com/api/films/1/"))
-                   .timeout(Duration.ofMinutes(2))
-                   .build();
-           HttpResponse<String> response = client
-                   .send(request, HttpResponse.BodyHandlers.ofString());
-
-           System.out.println(response);
-           System.out.println(response.body());
-           Pelicula peli = new Gson().fromJson(response.body(), Pelicula.class);
+       System.out.print("De la 1 hasta la 6, eleji una peli de Star War: ");
+       Scanner lectura = new Scanner(System.in);
+       int numeroDePelicula = Integer.valueOf(lectura.nextLine());
+           Pelicula peli = new BuscarPelicula().busquedaPelicula(numeroDePelicula);
            System.out.println("GSON es el sigueinte:");
            System.out.println(peli);
-       } catch (Exception e) {
-           throw new RuntimeException("No encontré esa película.");
-       }
    }
 }
